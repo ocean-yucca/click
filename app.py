f23e06c08200fa4e08d8c543986598594bba7ea5
Lin Chen <lchen@Mac.fios-router.home>
2025-12-09 10:03:58 -0500

    Init

---
+++ app.py
@@ -0,0 +1,27 @@
+import dash
+from dash import html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+server = app.server
+
+num_click = 0
+
+app.layout = html.Div([
+    html.Button('Click', id = 'click'),
+    html.Div('0', id = 'content')
+        ])
+
+@app.callback(
+    Output('content', 'children'),
+    Input('click', 'n_clicks'))
+def display_hover_data(c):
+    if c:
+        global num_click
+        num_click += 1
+        return num_click
+    else:
+        return 0
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
