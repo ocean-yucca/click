73a98a0acc628e743a866c994bd1dfa1f5010c35
Lin Chen <lchen@Mac.fios-router.home>
2025-12-09 14:46:40 -0500

    3rd version

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ num_click = 0
 app.layout = html.Div([
     html.Button('Click', id = 'click'),
     html.Div('0', id = 'content'),
-    html.Div('2nd Version')
+    html.Div('3rd Version')
         ])
 
 @app.callback(
